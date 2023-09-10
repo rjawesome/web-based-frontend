d218c896a665a99724eacc27f9dc180970f0a2a5
syossu <jiang.alexander@gmail.com>
2023-09-10 14:41:11 -0700

    problems --> challenges

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -7,8 +7,8 @@
         <h1 class="text-white text-3xl font-bold mb-4">Welcome to Web Based!</h1>
         <p class="text-white mb-6">Login/Click on Profile Picture to set your ID.</p>
         <p class="text-white mb-6">Go to the start page and click on the "start" button to start your 4 hour window.</p>
-        <p class="text-white mb-6">Proceed to problems when you have started. Good luck!</p>
-        <a href="{challengePageUrl}" class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded">Go to Problems</a>
+        <p class="text-white mb-6">Proceed to Challenges when you have started. Good luck!</p>
+        <a href="{challengePageUrl}" class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded">Go to Challenges</a>
     </div>
     
 </div>
